2d6c339504dddfaa987b6de3951074797ca623df
IntisarKabir <75949278+IntisarKabir@users.noreply.github.com>
2021-06-24 10:57:20 +0600

    Update lstm_spam_detection1.1.py
    empty main

--- lstm_spam_detection1.1.py
+++ lstm_spam_detection1.1.py
@@ -1,52 +1,5 @@
-#lstm_spam_detection1.1.py
+#main 
 
 
 
-import pandas as pd
-
-dataset_url = "https://raw.githubusercontent.com/IntisarKabir/ml-datasets/main/commonDatasets/spam.csv"
-df = pd.read_csv(dataset_url , encoding='latin-1')
-print(df.shape)
-df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1, inplace=True)
-df.columns = ["SpamHam","Tweet"]
-print(df.shape)
-
-
-!pip install contractions #pip 
-import contractions
-
-
-import numpy as np
-import re
-import collections
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-plt.style.use('dark_background')
-
-
-
-import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
-import warnings
-warnings.simplefilter(action='ignore', category=Warning)
-
-
-
-import keras
-from keras.layers import Dense, Embedding, LSTM, Dropout
-from keras.models import Sequential
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-import pickle
-
-
-
-sns.countplot(df["SpamHam"])
-
-
-
-
-#test branch
 
