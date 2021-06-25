75ac079a50abf773ee15a13e35858a0a6cdd7f38
IntisarKabir <75949278+IntisarKabir@users.noreply.github.com>
2021-06-25 14:11:29 +0600

    Update commonSnippets.py

--- commonSnippets.py
+++ commonSnippets.py
@@ -7,22 +7,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
+plt.style.use('fivethirtyeight')
+import seaborn as sns; sns.set()
+
+
 from datetime import datetime
-from pandas import Series
+
+
+
 import warnings
 warnings.filterwarnings("ignore")
-plt.style.use('fivethirtyeight')
+
 
 
 
 
 
 from google.colab import files
-
 uploaded = files.upload()
 
 
 
+from pandas import Series
+
+
+
+from keras.layers import SimpleRNN, Embedding, Dense, LSTM
+from keras.models import Sequential
 
 
 
