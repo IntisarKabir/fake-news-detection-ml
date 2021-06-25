6f01e9577795aab7fff7b6d4eb101dc269622da5
IntisarKabir <75949278+IntisarKabir@users.noreply.github.com>
2021-06-25 20:59:57 +0600

    Update fnd.py

--- fnd.py
+++ fnd.py
@@ -20,5 +20,75 @@ def remove_noise_from_string(str):
 
 
 
+def format_string(str):
+    v=str.replace("??", " ? ").replace("।।", " | ").replace("॥", " | ").replace("!!", " ! ").replace("||", " ")
+    v=v.replace("…", " ").replace("-", " - ").replace("/", " / ").replace(",", " ")   
+    v=v.replace("?", " ? ").replace("'", "_ ").replace("'", " _").replace("/", " ")
+    v=v.replace("#", " # ").replace("@", " @ ").replace("$", "$ ").replace("*", " * ").replace("&", " & ")
+    v=v.replace("+", " + ").replace("=", " = ").replace("{", "___ ").replace("}", " ___").replace("[", "___ ").replace("]", " ___")
+    v=v.replace("(", " ___ ").replace(")", " ___ ")
+    v=v.replace(":", " : ") #.replace("য়", "y")  #য়
+    v=v.replace("!", " ! ").replace("|", " । ").replace("।", " | ")
+    v=v.replace("‘", "_ ").replace("’", " _").replace('“',"__ ").replace('”'," __").replace("`", "_").replace('"', "__ ").replace('"' , " __")
+    v=v.split("!|?")
+    v=" ".join(v).strip()
+    return ' '+v
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
